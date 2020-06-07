11066d4a384a2cff0fa59f530b7d752fbf235b21
sherinshoni <39957008+sherinshoni@users.noreply.github.com>
2020-06-07 18:21:42 +0530

    Update words_rectangle.py

--- words_rectangle.py
+++ words_rectangle.py
@@ -1,14 +1,14 @@
 def rect_frame(element_list):
-	length=max(len(i) for i in element_list)
-	print("*"*(length+4))
-	for i in element_list:
-		print("*"+" "+i+" "*((length+4)-len(i)-3)+"*")
-	print("*"*(length+4))
+    length=max(len(i) for i in element_list)
+    print("*"*(length+4))
+    for i in element_list:
+        print("*"+" "+i+" "*((length+4)-len(i)-3)+"*")
+    print("*"*(length+4))
 	
 n=int(input("Enter the number of words"))
 list1=[]
 for i in range(0,n):
-	word=input("Enter the word")
-	list1.append(word)
+    word=input("Enter the word")
+    list1.append(word)
 rect_frame(list1)
 
