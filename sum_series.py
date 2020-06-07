6cc656b9838ba13b0644e89b0cc2f00377ae4f74
sherinshoni <39957008+sherinshoni@users.noreply.github.com>
2020-06-07 13:21:30 +0530

    Update and rename series_sum.py to sum_series.py

---
+++ sum_series.py
@@ -0,0 +1,11 @@
+def sum_series(num):
+	sum=0
+	fact=1
+	for i in range(1,num+1):
+		fact=fact*i
+		sum=sum+(i/fact)
+	return sum
+
+num=int(input("Enter the number"))
+sum=sum_series(num)
+print("Sum of the series = "+sum)
