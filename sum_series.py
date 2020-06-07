3e5fe83fb0e20c2839e9ae81bd3d9fc52baec8f6
sherinshoni <39957008+sherinshoni@users.noreply.github.com>
2020-06-07 18:23:18 +0530

    Update sum_series.py

--- sum_series.py
+++ sum_series.py
@@ -1,10 +1,10 @@
 def sum_series(num):
-	sum=0
-	fact=1
-	for i in range(1,num+1):
-		fact=fact*i
-		sum=sum+(i/fact)
-	return sum
+    sum=0
+    fact=1
+    for i in range(1,num+1):
+        fact=fact*i
+	sum=sum+(i/fact)
+    return sum
 
 num=int(input("Enter the number"))
 sum=sum_series(num)
