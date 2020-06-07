db13a4e10a7b6a4f1dc3e9d9b33e7ff23f794a65
sherinshoni <39957008+sherinshoni@users.noreply.github.com>
2020-06-07 17:45:57 +0530

    Update check_date.py

--- check_date.py
+++ check_date.py
@@ -1,1 +1,33 @@
-print("Enter day 
+current_year=2020
+current_month=06
+current_day=07
+
+d=print("Enter day in DD format : ")
+m=print("Enter month in MM format : ")
+y=print("Enter year in YYYY format : ")
+
+if (y>=0 and 0<m<13 and 0<d<32):
+	if (y%4==0 and (y%100!=0 or y%400==0) and m==2):
+		if d<=29:
+	        valid=True
+	    else:
+			valid=False
+	elif (m in [1,3,5,7,8,10,12] and d<=31):
+		valid=True
+	elif (m in [4,6,9,11] and d<=30):
+		valid=True
+	else:
+		valid=False
+	if (valid==True and y<=current_year and m<=current_month and d<=current_day):
+		valid=True
+	else:
+		valid=False
+else:
+	valid=False
+if (valid==True):
+	print("VALID DATE")
+else:
+	print("INVALID DATE")
+		
+			
+			
